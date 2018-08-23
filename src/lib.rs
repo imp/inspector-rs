85ea2d1df00c67083c1ffb6dace2d1102b20428c
Cyril Plisko <cyril.plisko@mountall.com>
2018-08-23 13:27:58 +0300

    Add top level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! Crate `inspector` extends popular data structures (such as `Option` and `Result`)
+//! with additional methods for inspecting their payload. It is inspired by the `Iterator::inspect`.
+//! Since no such methods are available by default on `Option` and `Result` types, this crate
+//! implements a new traits for these types, which augment the respective types with various
+//! inspection capabilities.
+
 #[cfg(feature = "iter")]
 mod iter;
 #[cfg(feature = "option")]
