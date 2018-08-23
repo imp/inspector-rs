8a97d9a8db73f793067aa966dd3c910aa31c7737
Cyril Plisko <cyril.plisko@mountall.com>
2018-08-23 13:46:18 +0300

    More docs

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,21 @@
 //! Since no such methods are available by default on `Option` and `Result` types, this crate
 //! implements a new traits for these types, which augment the respective types with various
 //! inspection capabilities.
+//!
+//! Implementation and availability of each trait is guarded by the dedicated feature, so that
+//! you can choose which one is available.
+//!
+//! # Features
+//! `option` - enables trait `OptionInspector`
+//! `result` - enables trait `ResultInspector`
+//! `iter` - enables trait `IterInspector` (broken at the moment)
+
+#![cfg_attr(
+    all(feature = "cargo-clippy", feature = "pedantic"),
+    warn(clippy_pedantic)
+)]
+#![cfg_attr(feature = "cargo-clippy", warn(use_self))]
+#![deny(warnings, missing_debug_implementations)]
 
 #[cfg(feature = "iter")]
 mod iter;
